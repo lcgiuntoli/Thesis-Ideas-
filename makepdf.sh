853d9a9e9a149dba5b817f7c127e21a707a76e7f
Neel Smith <dnsmith.neel@gmail.com>
2021-05-18 11:04:14 -0400

    Add initial PDF

--- makepdf.sh
+++ makepdf.sh
@@ -1,3 +1,3 @@
 # Requires pandoc and xelatex.
 PANDOC=`which pandoc`
-$PANDOC  --output "thesis.pdf"  --variable mainfont="Gentium Plus"  --template="typesetting/style/template.tex"  --include-in-header="typesetting/style/preamble.tex" --toc --variable=fontsize:12pt --variable=documentclass:report  --pdf-engine=xelatex "Chapters"/*.md "Appendices"/*md  "Chapters/metadata.yml"  --verbose
+$PANDOC  --output "thesis.pdf"  --variable mainfont="Gentium Plus"  --template="typesetting/style/template.tex"  --include-in-header="typesetting/style/preamble.tex" --toc --variable=fontsize:12pt --variable=documentclass:report  --pdf-engine=xelatex "Chapters"/*.md  "Chapters/metadata.yml"  --verbose
